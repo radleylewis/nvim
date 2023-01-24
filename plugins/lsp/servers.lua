0fd54be6711c422010c6ea28c7368aa1e639d8d0
Radley Sidwell-Lewis <radley.lewis@gmail.com>
2023-01-24 20:43:53 +0700

    fix: require tsserver

--- plugins/lsp/servers.lua
+++ plugins/lsp/servers.lua
@@ -37,8 +37,8 @@ local on_attach = function(client, bufnr)
 	-- typescript specific keymaps (e.g. rename file and update imports)
 	if client.name == "tsserver" then
 		keymap.set("n", "<leader>rf", ":TypescriptRenameFile<CR>") -- rename file and update imports
-		keymap.set("n", "<leader>oi", ":TypescriptOrganizeImports<CR>") -- organize imports (not in youtube nvim video)
-		keymap.set("n", "<leader>ru", ":TypescriptRemoveUnused<CR>") -- remove unused variables (not in youtube nvim video)
+		keymap.set("n", "<leader>oi", ":TypescriptOrganizeImports<CR>") -- organize imports
+		keymap.set("n", "<leader>ru", ":TypescriptRemoveUnused<CR>") -- remove unused variables
 	end
 
 	if client.name == "pyright" then
@@ -90,6 +90,12 @@ typescript.setup({
 		capabilities = capabilities,
 		on_attach = on_attach,
 	},
+})
+
+-- configure css server
+lspconfig.tsserver.setup({
+	capabilities = capabilities,
+	on_attach = on_attach,
 })
 
 -- configure css server
