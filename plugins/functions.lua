b869ebfbb05566e74f87f13e4664a60c2890fdf8
Radley Sidwell-Lewis <radley.lewis@gmail.com>
2023-02-28 05:38:09 +0800

    fix: git helper fn

--- plugins/functions.lua
+++ plugins/functions.lua
@@ -1,9 +1,8 @@
 --  Useful Functions
 
+vim.api.nvim_set_option("completefunc", "luacomplete#Complete")
+
 -- Removes the Head Changes in a git diff
-function RemoveGitConflictMarkers(branch)
-	vim.api.nvim_buf_set_lines(0, 0, -1, false, vim.fn.getline(1, "$"))
-	vim.api.nvim_command(
-		[[%s/\v(<<<<<<< HEAD\n(.|\n)*\n=======\n(.|\n)*\n\|=======\n(.|\n)*\n>>>>>>> ]] .. branch .. [[\n//g]]
-	)
+function GitDiffRemove()
+	vim.api.nvim_command([[%s/<<<<<<< HEAD\n\_.\{-}\n=======\n//]])
 end
