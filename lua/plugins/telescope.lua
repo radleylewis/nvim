158dddf4210b3feaa038113d9d1613f838c5b718
Radley E Siwell-Lewis <radley.lewis@gmail.com>
2023-10-27 16:23:10 +0700

    chore: enable telescope previewer

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -14,16 +14,16 @@ local config = function()
 		pickers = {
 			find_files = {
 				theme = "dropdown",
-				previewer = false,
+				previewer = true,
 				hidden = true,
 			},
 			live_grep = {
 				theme = "dropdown",
-				previewer = false,
+				previewer = true,
 			},
 			buffers = {
 				theme = "dropdown",
-				previewer = false,
+				previewer = true,
 			},
 		},
 	})
