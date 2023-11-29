5841c9b2feea156eadfc72717777e7b04134e406
Radley E Siwell-Lewis <radley.lewis@gmail.com>
2023-11-29 08:41:26 +0700

    refactor: switch lualine to bottom (tmux to top)

--- lua/plugins/lualine-nvim.lua
+++ lua/plugins/lualine-nvim.lua
@@ -1,9 +1,19 @@
 local config = function()
 	require("lualine").setup({
 		options = {
-			theme = "catppuccin",
+			theme = "auto",
 			globalstatus = true,
+			component_separators = { left = "|", right = "|" },
+			section_separators = { left = "", right = "" },
 		},
+		sections = {
+			lualine_a = { "mode" },
+			lualine_b = { "buffers" },
+			lualine_x = { "encoding", "fileformat", "filetype" },
+			lualine_y = { "progress" },
+			lualine_z = { "location" },
+		},
+		tabline = {},
 	})
 end
 
