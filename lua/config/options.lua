afa197626e0d05660423d5da9712fad28a9e3a7b
Hemanth KS <161621694+hemanthgowdaks@users.noreply.github.com>
2024-03-29 08:23:47 +0530

    Update showmode in ~/.config/nvim/config/options.lua
    i  have changed opt.showmode = true 
    when it was false had to manually do :set showmode to just see if i am in insert of visual mode

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -43,7 +43,7 @@ opt.modifiable = true
 opt.guicursor =
 	"n-v-c:block,i-ci-ve:block,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175"
 opt.encoding = "UTF-8"
-opt.showmode = false
+opt.showmode = true
 
 -- folds
 opt.foldmethod = "expr"
