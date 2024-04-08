2d50bc6d41b72f3281bac5878c116b67f2d7dfe5
Radley E Siwell-Lewis <radley.lewis@proton.me>
2024-04-08 16:45:09 +0700

    refactor: split window and goto_definition

--- lua/util/lsp.lua
+++ lua/util/lsp.lua
@@ -8,6 +8,7 @@ M.on_attach = function(client, bufnr)
 	mapkey("<leader>fd", "Lspsaga finder", "n", opts) -- go to definition
 	mapkey("<leader>gd", "Lspsaga peek_definition", "n", opts) -- peak definition
 	mapkey("<leader>gD", "Lspsaga goto_definition", "n", opts) -- go to definition
+	mapkey("<leader>gS", "vsplit | Lspsaga goto_definition", "n", opts) -- go to definition
 	mapkey("<leader>ca", "Lspsaga code_action", "n", opts) -- see available code actions
 	mapkey("<leader>rn", "Lspsaga rename", "n", opts) -- smart rename
 	mapkey("<leader>D", "Lspsaga show_line_diagnostics", "n", opts) -- show  diagnostics for line
