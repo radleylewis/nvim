865ddb40fe3203378b417efd31404504188c0127
Radley Edward Sidwell-Lewis <radley.lewis@gmail.com>
2024-09-23 10:28:35 +0700

    fix: ts_ls looking in wrong root dir

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -79,7 +79,7 @@ local config = function()
 				indentSize = 2,
 			},
 		},
-		root_dir = lspconfig.util.root_pattern("package.json", "tsconfig.json", ".git"),
+		-- root_dir = lspconfig.util.root_pattern("package.json", "tsconfig.json", ".git"),
 	})
 
 	-- bash
