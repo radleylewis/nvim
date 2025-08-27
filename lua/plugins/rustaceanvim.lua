4a680646fc83d6fe664e74cb7189a474fae3026a
Radley Edward Sidwell-Lewis <radley.lewis@gmail.com>
2025-08-27 15:32:56 +0200

    chore: consolidate rust config

--- lua/plugins/rustaceanvim.lua
+++ lua/plugins/rustaceanvim.lua
@@ -4,7 +4,45 @@
 -- LINKS :
 --   > github : https://github.com/mrcjkb/rustaceanvim
 -- ================================================================================================
-local config = require("servers.rust-analyzer").config
+local get_codelldb_adapter = function()
+	local mason_registry = require("mason-registry")
+	if mason_registry.is_installed("codelldb") then
+		local codelldb = mason_registry.get_package("codelldb")
+		local extension_path = codelldb:get_install_path() .. "/extension/"
+		local codelldb_path = extension_path .. "adapter/codelldb"
+		local base_path = extension_path .. "lldb/lib/liblldb"
+		---@diagnostic disable-next-line: undefined-field (os_uname)
+		local this_os = vim.uv.os_uname().sysname
+		local liblldb_path = base_path .. (this_os == "Linux" and ".so" or ".dylib")
+		local cfg = require("rustaceanvim.config")
+		return cfg.get_codelldb_adapter(codelldb_path, liblldb_path)
+	end
+end
+
+local config = function()
+	local settings = {
+		tools = {
+			hover_actions = {
+				auto_focus = true,
+			},
+		},
+		server = {
+			settings = {
+				["rust-analyzer"] = {
+					cargo = {
+						allFeatures = true,
+					},
+				},
+			},
+		},
+		dap = {},
+	}
+	local ok, adapter = pcall(get_codelldb_adapter)
+	if ok then
+		settings.dap.adapter = adapter
+	end
+	vim.g.rustaceanvim = settings
+end
 
 return {
 	"mrcjkb/rustaceanvim",
